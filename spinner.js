fddd4c9a592f58345b8417e455c3d822c4884455
FlowerPowerz <maia3303@outlook.com>
2017-05-15 18:12:44 -0500

    Create spinner.js

---
+++ spinner.js
@@ -0,0 +1,15 @@
+function Spinner (s) {
+  this.dec = .15;
+  this.rot = 0;
+  this.vel = 0;
+  this.spi = s;
+  this.pos = {x: window.innerWidth*0.2, y: window.innerHeight*0.2};
+};
+Spinner.prototype.update = function () {
+  this.vel += this.rot;
+  if(this.vel > this.dec) { this.vel -= this.dec } else { this.vel = 0 };
+  this.spi.style.transform = "rotate(somedeg)".replace("some",this.rot);
+  this.spi.style.position = "absolute";
+  this.spi.style.left = this.pos.x+"px";
+  this.spi.style.top = this.pos.y+"px";
+};
