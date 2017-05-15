e1416b1fec9bd5848b8802ccae4a31f2229b18c2
FlowerPowerz <maia3303@outlook.com>
2017-05-15 18:13:46 -0500

    Update spinner.js

--- spinner.js
+++ spinner.js
@@ -4,6 +4,8 @@ function Spinner (s) {
   this.vel = 0;
   this.spi = s;
   this.pos = {x: window.innerWidth*0.2, y: window.innerHeight*0.2};
+  this.spi.addEventListener("touchstart", this.spin.bind(this));
+  this.spi.addEventListener("mousedown", this.spin.bind(this));
 };
 Spinner.prototype.update = function () {
   this.vel += this.rot;
@@ -12,4 +14,8 @@ Spinner.prototype.update = function () {
   this.spi.style.position = "absolute";
   this.spi.style.left = this.pos.x+"px";
   this.spi.style.top = this.pos.y+"px";
+};
+
+Spinner.prototype.spin = function () {
+  this.vel += this.dec*10;
 };
