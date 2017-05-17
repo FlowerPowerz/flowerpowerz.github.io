4597e02bfe133f1ab2f8be37d3cbfe82f86b9561
FlowerPowerz <maia3303@outlook.com>
2017-05-17 06:57:29 -0500

    Update spinner.js

--- spinner.js
+++ spinner.js
@@ -1,29 +1,47 @@
-function Spinner (s) {
-  this.dec = .15;
-  this.rot = 0;
-  this.vel = 0;
-  this.spi = s;
-  this.pos = {x: window.innerWidth*0.2, y: window.innerHeight*0.2};
-  this.spi.addEventListener("touchstart", this.spin.bind(this));
-  this.spi.addEventListener("click", this.spin.bind(this));
+var Entity = (function () {
+    function Entity(_element) {
+        this.x = 0;
+        this.y = 0;
+        this.velX = 0;
+        this.velY = 0;
+        this.velRot = 0;
+        this.rotation = 0;
+        this.element = null;
+        this.spin = function (e) {
+            this.velRot += 3;
+        };
+        this.update = function () {
+            this.x += this.velX;
+            this.y += this.velY;
+            this.rotation += this.velRot;
+            this.velRot -= 0.05;
+            this.element.style.position = "absolute";
+            this.element.style.left = this.x + "px";
+            this.element.style.top = this.y + "px";
+            this.element.style.transform = "rotate(" + this.rotation + "deg)";
+        };
+        this.element = _element;
+        this.element.addEventListener("click", this.spin.bind(this), true);
+    }
+    ;
+    return Entity;
+}());
+;
+var entities = [];
+var Draw = function () {
+    for (var _i = 0, entities_1 = entities; _i < entities_1.length; _i++) {
+        var i = entities_1[_i];
+        i.update();
+    }
+    ;
+    setTimeout(Draw, 1000 / 60);
 };
-Spinner.prototype.update = function () {
-  this.rot += this.vel;
-  if(this.vel > this.dec) { this.vel -= this.dec } else { this.vel = 0 };
-  this.spi.style.transform = "rotate(somedeg)".replace("some",this.rot);
-  this.spi.style.position = "absolute";
-  this.spi.style.left = this.pos.x+"px";
-  this.spi.style.top = this.pos.y+"px";
+window.onload = function () {
+    var htmlspinners = document.getElementsByClassName("spinner");
+    for (var i = htmlspinners.length - 1; i >= 0; i--) {
+        var a = new Entity(htmlspinners[i]);
+        entities.push(a);
+    }
+    ;
+    setTimeout(Draw, 1000 / 60);
 };
-
-Spinner.prototype.spin = function () {
-  this.vel += this.dec*10;
-};
-
-window.onload = function(){
-  var spinners = document.getElementsByClassName("spinner");
-  for (var i = 0; i < spinners.length; i ++ ){
-    var spinner = (new Spinner(spinners[i]));
-    requestAnimationFrame(spinner.update.bind(spinner));
-  }
-}
